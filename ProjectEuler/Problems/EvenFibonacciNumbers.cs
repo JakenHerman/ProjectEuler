7a5f689701c394b2330722f31a287b42034d8d08
Jaken Herman <jaken@Brooklyns-MacBook-Pro.local>
2017-12-27 21:51:02 -0600

    problem 2 complete

---
+++ ProjectEuler/Problems/EvenFibonacciNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+    public class EvenFibonacciNumbers
+    {
+        public int sumOfEvenValuedTerms()
+        {
+            List<int> fibList = new List<int>();
+            int i = 0;
+            int sum = 0;
+            while(fibonacci(i) < 4000000)
+            {
+                fibList.Add(fibonacci(i));
+                i++;
+            }
+            foreach(int x in fibList)
+            {
+                if(x % 2 == 0)
+                {
+                    sum += x;   
+                }
+            }
+            return sum;
+        }
+
+        public int fibonacci(int n){
+            if(n <= 1)
+            {
+                return n;
+            }
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ProjectEuler.Problems;
+
+namespace ProjectEuler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            EvenFibonacciNumbers efn = new EvenFibonacciNumbers();
+            Console.WriteLine(efn.sumOfEvenValuedTerms());
+        }
+    }
+}
